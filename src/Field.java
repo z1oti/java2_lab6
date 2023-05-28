90e063c5535793f453e937251d5d5f013f16e2a6
z1oti <savchuk.zlata2003@gmail.com>
2023-05-29 00:01:39 +0300

    initial commit

---
+++ src/Field.java
@@ -0,0 +1,86 @@
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+@SuppressWarnings("serial")
+public class Field extends JPanel {
+    // Флаг приостановленности движения
+    private boolean paused;
+    private boolean pausedGreen;
+    // Динамический список скачущих мячей
+    private ArrayList<BouncingBall> balls = new ArrayList<BouncingBall>(10);
+    // Класс таймер отвечает за регулярную генерацию событий ActionEvent
+    // При создании его экземпляра используется анонимный класс,
+    // реализующий интерфейс ActionListener
+    private Timer repaintTimer = new Timer(10, new ActionListener() {
+        public void actionPerformed(ActionEvent ev) {
+            // Задача обработчика события ActionEvent - перерисовка окна
+            repaint();
+        }
+    });
+
+    // Конструктор класса BouncingBall
+    public Field() {
+        // Установить цвет заднего фона белым
+        setBackground(Color.WHITE);
+        // Запустить таймер
+        repaintTimer.start();
+    }
+
+    // Унаследованный от JPanel метод перерисовки компонента
+    public void paintComponent(Graphics g) {
+        // Вызвать версию метода, унаследованную от предка
+        super.paintComponent(g);
+        Graphics2D canvas = (Graphics2D) g;
+        // Последовательно запросить прорисовку от всех мячей из списка
+        for (BouncingBall ball : balls) {
+            ball.paint(canvas);
+        }
+    }
+    // Метод добавления нового мяча в список
+    public void addBall() {
+        //Заключается в добавлении в список нового экземпляра BouncingBal
+        //Всю инициализацию положения, скорости, размера, цвета
+        // BouncingBall выполняет сам в конструкторе
+        balls.add(new BouncingBall(this));
+    }
+    // Метод синхронизированный, т.е. только один поток может
+    // одновременно быть внутри
+    public synchronized void pause() {
+        // Включить режим паузы
+        paused = true;
+    }
+    public synchronized void pauseGreen() {
+        // Включить режим паузы для зеленых
+        pausedGreen = true;
+    }
+    // Метод синхронизированный, т.е. только один поток может
+    // одновременно быть внутри
+    public synchronized void resume() {
+        // Выключить режим паузы
+        paused = false;
+        pausedGreen = false;
+        // Будим все ожидающие продолжения потоки
+        notifyAll();
+    }
+    // Синхронизированный метод проверки, может ли мяч двигаться
+    // (не включен ли режим паузы?)
+    public synchronized void canMove(BouncingBall ball) throws
+            InterruptedException {
+        if (paused) {
+            // Если режим паузы включен, то поток, зашедший
+            // внутрь данного метода, засыпает
+            wait();
+        }
+        // добавляем условие для зеленого спектра
+        if (pausedGreen && (2*(ball.getRed()+ball.getBlue())) < ball.getGreen()){
+            wait();
+        }
+    }
+}
---
+++ src/MainFrame.java
@@ -0,0 +1,95 @@
+
+import java.awt.BorderLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+@SuppressWarnings("serial")
+public class MainFrame extends JFrame {
+    // Константы, задающие размер окна приложения, если оно
+// не распахнуто на весь экран
+    private static final int WIDTH = 700;
+    private static final int HEIGHT = 500;
+    private JMenuItem pauseMenuItem;
+    private JMenuItem resumeMenuItem;
+    private JMenuItem pauseGreenItem;
+    // Поле, по которому прыгают мячи
+    private Field field = new Field();
+    // Конструктор главного окна приложения
+    public MainFrame() {
+        super("Программирование и синхронизация потоков");
+        setSize(WIDTH, HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+// Отцентрировать окно приложения на экране
+        setLocation((kit.getScreenSize().width - WIDTH)/2,
+                (kit.getScreenSize().height - HEIGHT)/2);
+// Установить начальное состояние окна развѐрнутым на весь экран
+        setExtendedState(MAXIMIZED_BOTH);
+// Создать меню
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        JMenu ballMenu = new JMenu("Мячи");
+        Action addBallAction = new AbstractAction("Добавить мяч") {
+            public void actionPerformed(ActionEvent event) {
+                field.addBall();
+                if (!pauseMenuItem.isEnabled() &&
+                        !resumeMenuItem.isEnabled()) {
+// Ни один из пунктов меню не являются
+// доступными - сделать доступным "Паузу"
+                    pauseMenuItem.setEnabled(true);
+                }
+            }
+        };
+        menuBar.add(ballMenu);
+        ballMenu.add(addBallAction);
+        JMenu controlMenu = new JMenu("Управление");
+        menuBar.add(controlMenu);
+        Action pauseAction = new AbstractAction("Приостановить движение"){
+            public void actionPerformed(ActionEvent event) {
+                field.pause();
+                pauseMenuItem.setEnabled(false);
+                resumeMenuItem.setEnabled(true);
+                pauseGreenItem.setEnabled(false);
+            }
+        };
+        pauseMenuItem = controlMenu.add(pauseAction);
+        pauseMenuItem.setEnabled(false);
+        Action resumeAction = new AbstractAction("Возобновить движение") {
+            public void actionPerformed(ActionEvent event) {
+                field.resume();
+                pauseMenuItem.setEnabled(true);
+                resumeMenuItem.setEnabled(false);
+                pauseGreenItem.setEnabled(true);
+            }
+        };
+        resumeMenuItem = controlMenu.add(resumeAction);
+        resumeMenuItem.setEnabled(false);
+
+        // метод для остановки зеленых
+        Action pauseGreen = new AbstractAction("Приостановить зеленые") {
+            public void actionPerformed(ActionEvent event) {
+                field.pauseGreen();
+                pauseMenuItem.setEnabled(true);
+                resumeMenuItem.setEnabled(true);
+                pauseGreenItem.setEnabled(false);
+            }
+        };
+        pauseGreenItem = controlMenu.add(pauseGreen);
+        pauseGreenItem.setEnabled(true);
+
+
+// Добавить в центр граничной компоновки поле Field
+        getContentPane().add(field, BorderLayout.CENTER);
+    }
+    // Главный метод приложения
+    public static void main(String[] args) {
+// Создать и сделать видимым главное окно приложения
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
